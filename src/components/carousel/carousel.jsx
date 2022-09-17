cfd281669fb57a984f1843c451011b761205581c
AzraYazid <azrayazidalkautsar@gmail.com>
2022-09-17 13:51:40 +0700

    update UI

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -2,6 +2,8 @@ import { Container } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 
 import Crs1 from '../../image/crs1.jpg';
+import Crs2 from '../../image/crs2.png';
+import Crs3 from '../../image/crs3.jpg';
 
 export default function Carosel() {
   return (
@@ -10,23 +12,23 @@ export default function Carosel() {
         <Carousel.Item>
           <img className="d-block w-100 rounded" src={Crs1} alt="First slide" />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            {/* <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100 rounded" src={Crs1} alt="Second slide" />
+          <img className="d-block w-100 rounded" src={Crs2} alt="Second slide" />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            {/* <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100 rounded" src={Crs1} alt="Third slide" />
+          <img className="d-block w-100 rounded" src={Crs3} alt="Third slide" />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            {/* <h3>Third slide label</h3> */}
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item>
